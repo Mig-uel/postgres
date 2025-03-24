503e0ba960f04233eba7756b4187abfe0d57553e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-23 20:58:25 -0400

    Add Express server with routes for posts management
    - Implement home route and posts route to fetch and display posts
    - Create form for adding new posts with latitude and longitude
    - Handle post creation and redirect to posts list

---
+++ social-network/index.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const { pool } = require('./db/index')
+
+const PORT = 3001
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  return res.json({
+    message: 'Home Route',
+    routes: '/, /posts',
+  })
+})
+
+app.get('/posts', async (req, res) => {
+  const { rows } = await pool.query(`
+      SELECT * FROM posts;
+    `)
+
+  return res.send(`
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>lng</th>
+            <th>lat</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          ${rows
+            .map((row) => {
+              return `
+              <tr>
+                <td>${row.id}</td>
+                <td>${row.lng}</td>
+                <td>${row.lat}</td>
+              </tr>
+            `
+            })
+            .join('')}
+        </tbody>
+
+      </table>
+      <form method="POST">
+            <h3>Create Post</h3>
+
+            <div>
+              <label>Lng</label>
+              <input type="number" name="lng" required step="0.001"/>
+            </div>
+            <div>
+              <label>Lat</label>
+              <input type="number" name="lat" required step="0.001" />
+            </div>
+            <button type="submit">Create Post</button>
+      </form>
+    `)
+})
+
+app.post('/posts', async (req, res) => {
+  const { lng, lat } = req.body
+
+  await pool.query(`INSERT INTO posts (lat, lng) VALUES ($1, $2)`, [lat, lng])
+
+  return res.redirect('/posts')
+})
+
+app.all('*', (req, res) => {
+  return res.redirect('/')
+})
+
+app.listen(PORT, () => {
+  console.log(`RUNNING ON PORT ${PORT}`)
+})
