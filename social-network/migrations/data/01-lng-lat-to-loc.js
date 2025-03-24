6df749d78ba0cbaa691d0403efa0d839442011fb
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-23 21:43:56 -0400

    Add migration script to update loc column in posts table
    - Set loc column using POINT(lng, lat) where loc is NULL
    - Ensures loc data is populated for existing posts

---
+++ social-network/migrations/data/01-lng-lat-to-loc.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const { pool } = require('../../db')
+
+pool
+  .query(
+    `
+    UPDATE posts
+    SET loc = POINT(lng, lat)
+    WHERE loc IS NULL;
+  `
+  )
+  .then((result) => {
+    console.log('-- UPDATE COMPLETE --')
+
+    pool.end()
+  })
+  .catch((err) => {
+    console.log(err)
+    pool.end()
+  })
