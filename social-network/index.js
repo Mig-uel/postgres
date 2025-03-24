8f3aae06a057c6b10a9920ce52a0427d97b38a7e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-23 21:04:55 -0400

    Add loc column to posts insertion in posts route
    - Update the posts creation logic to include the new 'loc' column
    - Store location as a string in the format '(lng, lat)'

--- social-network/index.js
+++ social-network/index.js
@@ -64,7 +64,12 @@ app.get('/posts', async (req, res) => {
 app.post('/posts', async (req, res) => {
   const { lng, lat } = req.body
 
-  await pool.query(`INSERT INTO posts (lat, lng) VALUES ($1, $2)`, [lat, lng])
+  // insert values into new loc column
+  await pool.query(`INSERT INTO posts (lat, lng, loc) VALUES ($1, $2, $3)`, [
+    lat,
+    lng,
+    `(${lng}, ${lat})`,
+  ])
 
   return res.redirect('/posts')
 })
