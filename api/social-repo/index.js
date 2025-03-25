1973be05708329389981824072bb8146e81f8fd8
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-24 22:47:51 -0400

    Refactor server setup to include database connection
    - Integrate database connection pooling with environment variables
    - Update server initialization to listen on port 3005

--- api/social-repo/index.js
+++ api/social-repo/index.js
@@ -1,14 +1,20 @@
-const express = require('express')
-const { userRouter } = require('./routes/users')
+const path = require('path')
+const app = require('./server')()
+const pool = require('./db/pool')
+require('dotenv').config({
+  path: path.resolve(__dirname, '.env'),
+})
 
-module.exports = () => {
-  const app = express()
-
-  // middleware
-  app.use(express.json())
-
-  // routes
-  app.use('/users', userRouter)
-
-  return app
+const options = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 }
+
+pool.connect(options)
+
+app.listen(3005, () => {
+  console.log('----- SERVER RUNNING -----')
+})
