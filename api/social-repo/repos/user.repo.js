f6eaa0e92a7306a0591de92f4ead519378df03d7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-25 14:07:10 -0400

    Add UserRepo class for user data access methods
    - Implemented find method to retrieve all users from the database
    - Placeholder methods for findById, insert, update, and delete

---
+++ api/social-repo/repos/user.repo.js
@@ -0,0 +1,21 @@
+const pool = require('../db/pool')
+
+class UserRepo {
+  static async find() {
+    return await pool.query(`
+        SELECT * FROM users;
+      `).rows
+  }
+
+  static async findById() {}
+
+  static async insert() {}
+
+  static async update() {}
+
+  static async delete() {}
+}
+
+module.exports = {
+  UserRepo,
+}
