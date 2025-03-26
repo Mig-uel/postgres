d9f0970d1fcbc40bd568f822c54db35ff3c4daa7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-26 19:01:48 -0400

    Add role and schema creation in user tests setup
    - Generate a random role name for PostgreSQL connection
    - Create a new role and schema for isolated testing
    - Run migrations in the newly created schema

--- api/social-repo/tests/routes/users.test.js
+++ api/social-repo/tests/routes/users.test.js
@@ -1,21 +1,71 @@
 const path = require('path')
+const { randomBytes } = require('crypto')
 const request = require('supertest')
 const pool = require('../../db/pool')
 const app = require('../../server')()
 const { UserRepo } = require('../../repos/user.repo')
+const format = require('pg-format')
+const { default: migrate } = require('node-pg-migrate')
 require('dotenv').config({
   path: path.resolve(__dirname, '../../.env'),
 })
 
-const options = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: 'socialnetwork-test',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-}
+beforeAll(async () => {
+  // randomly generate a role name to connect to PG as
+  const roleName = 't' + randomBytes(4).toString('hex')
 
-beforeAll(() => pool.connect(options))
+  // connect to PG as usual
+  await pool.connect({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: 'socialnetwork-test',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  })
+
+  // create a new role
+  await pool.query(
+    `
+      CREATE ROLE ${roleName} WITH LOGIN PASSWORD '${roleName}';
+    `
+  )
+
+  // create a schema with the same name
+  await pool.query(
+    `
+      CREATE SCHEMA ${roleName} AUTHORIZATION ${roleName};
+    `
+  )
+
+  // disconnect entirely from PG
+  await pool.close()
+
+  // run migration in the new schema
+  await migrate({
+    databaseUrl: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: 'socialnetwork-test',
+      user: roleName,
+      password: roleName,
+    },
+    dir: 'migrations',
+    direction: 'up',
+    log: () => {},
+    noLock: true,
+    schema: roleName,
+  })
+
+  // connect to pg a the newly created role
+
+  return await pool.connect({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: 'socialnetwork-test',
+    user: roleName,
+    password: roleName,
+  })
+})
 afterAll(() => {
   pool.close()
 })
